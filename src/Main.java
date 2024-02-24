39ca497f1f1a1f03d74ce52bfdf1a24850be9aa9
Gleb Laychenkov <racers2no95@mail.ru>
2024-02-24 18:46:22 +0400

    Initial commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        int balans = 100;
-        int popolnenie = 300;
+        int balance = 100;
+        int replenishment = 1100;
 
         int bonus;
-        if (popolnenie >= 1000) {
-            bonus = popolnenie / 100;
+        if (replenishment >= 1000) {
+            bonus = replenishment / 100;
         } else {
             bonus = 0;
         }
 
-        int itogo = (balans + popolnenie) + bonus;
+        int total = (balance + replenishment) + bonus;
 
-        System.out.println("Начальный баланс на счету " + balans + " рублей");
-        System.out.println("Пополнение счета на " + popolnenie + " рублей");
+        System.out.println("Начальный баланс на счету " + balance + " рублей");
+        System.out.println("Пополнение счета на " + replenishment + " рублей");
         System.out.println("Начислено дополнительно " + bonus + " рублей");
-        System.out.println("Итого на балансе " + itogo + " рублей");
+        System.out.println("Итого на балансе " + total + " рублей");
     }
 }
